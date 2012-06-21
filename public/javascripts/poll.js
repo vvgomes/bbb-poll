59d3a2df1f62cb82aa88d163639f1cc0878f52d0
Vinicius Gomes <vvgomess@gmail.com>
2012-06-21 14:36:24 -0300

    disabling submit by default

--- public/javascripts/poll.js
+++ public/javascripts/poll.js
@@ -4,12 +4,14 @@ function makeCandidateSelectable(dom) {
     avatars.children().removeClass('selected');
     var id = $(this).children().first().addClass('selected').attr('data-id');
     dom.selected().val(id);
+    dom.submit().removeAttr('disabled').val('Envie seu voto agora');
   });
 }
 
 var dom = {
   avatars: function(){ return $('.selectable'); },
-  selected: function(){ return $('input[name="selected_id"]'); }
+  selected: function(){ return $('input[name="selected_id"]'); },
+  submit: function(){ return $('#vote input[type="submit"]'); }
 };
 
 $(document).ready(function(){
--- spec/javascripts/pollSpec.js
+++ spec/javascripts/pollSpec.js
@@ -1,17 +1,35 @@
-describe('candidate avatar', function() {
-  it('should be selectable', function() {
-    var dom = fakeDom();
+describe('poll form', function() {
+  var dom;
+
+  beforeEach(function() {
+    dom = fakeDom();
     makeCandidateSelectable(dom);
+  });
+
+  it('should allow candidate selection', function() {
     dom.avatars().trigger('click');
     expect(dom.selected().val()).toBe('99');
+  });
+
+  it('should keep the submit button disabled by default', function() {
+    expect(dom.submit().attr('disabled')).toBeTruthy();
+    expect(dom.submit().val()).toBe('Escolha um candidato');
+  });
+
+  it('should enable the submit button after selection', function() {
+    dom.avatars().trigger('click');
+    expect(dom.submit().attr('disabled')).toBeFalsy();
+    expect(dom.submit().val()).toBe('Envie seu voto agora');
   });
 
   function fakeDom() {
     var avatars = $('<div><div data-id="99"></div></div>');
     var selected = $('<input value="">');
+    var submit = $('<input disabled value="Escolha um candidato">');
     return {
       avatars: function(){ return avatars; },
-      selected: function(){ return selected; }
+      selected: function(){ return selected; },
+      submit: function(){ return submit; }
     };
   }
 });
